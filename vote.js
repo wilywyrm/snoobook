1c15816a13aa487ccca05f86f8e4d15bca49eefc
wilywyrm <rufei.z@gmail.com>
2013-10-05 15:54:50 -0500

    initial commit

---
+++ vote.js
@@ -0,0 +1,12 @@
+function dislike() {
+	// firebase add dislike by user to post by ID if not already in list
+	// firebase increment dislike counter
+	// possibly decrement all posts by user with that image/keyword in it for user preferability
+}
+
+function like(){
+	// firebase add likes from all users of that post
+	// firebase add like by user to post by ID if not already in list
+	// firebase increment like counter
+	// possibly increment all posts by user with that image/keyword in it for user preferability
+}
