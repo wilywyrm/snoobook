bcda48e2d9158194a09cd81da92049583968788c
error74 <howard19940402@gmail.com>
2013-10-06 02:16:23 -0500

    half baked scraping
    asdf

--- fbtoreddit.js
+++ fbtoreddit.js
@@ -1,13 +1,27 @@
-function scrape(){
-	
+function scrape(response){
+	for(var a=0; a < response.data.length; a++)
+	{
+		var HTML = [];
+		//console.log(a);
+		if(response.data[a].hasOwnProperty('source') == true){
+			HTML.push("<div>" + a + response.data[a].from.name + "</div> ">)
+			//console.log(a +":"+response.data[a].from.name);
+			//console.log(response.data[a].id + '\n');
+		}
+		else{
+			//console.log(a+":"+response.data[a].from.name);
+			//console.log(response.data[a].message + '\n');
+		}
+		
+	}
 }
-
 
 function init() {
     console.log('Welcome!  Fetching your information.... ');
     FB.api('/me/home', {access_token: accessToken}, function(response) {
-   	  console.log(response);
-      $('#siteTable').text(response);
+    	scrape(response);
+   	  	console.log(response);
+      	$('#siteTable').text(response);
     });
     FB.api('/me', function(response){
     	userID = response.id;
