9d5e86dc39f898257b96034baabd02152ae4a8fa
wilywyrm <rufei.z@gmail.com>
2013-10-05 20:44:43 -0500

    separated out some functions

--- fbtoreddit.js
+++ fbtoreddit.js
@@ -2,4 +2,15 @@ function scrape(){
 	
 }
 
-function 
+
+function init() {
+    console.log('Welcome!  Fetching your information.... ');
+    FB.api('/me/home', {access_token: accessToken}, function(response) {
+   	  console.log(response);
+      $('#siteTable').text(response);
+    });
+    FB.api('/me', function(response){
+    	userID = response.id;
+    	console.log('Good to see you, ' + response.name + '.'); // ha you're a number
+    });
+}
