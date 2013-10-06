4ad3353b83356e018719b36678ecf42d081a2cf3
wilywyrm <rufei.z@gmail.com>
2013-10-06 05:50:16 -0500

    initial firebase usage for persistent dislikes

--- fbtoreddit.js
+++ fbtoreddit.js
@@ -1,9 +1,24 @@
+function init() {
+    console.log('Welcome!  Fetching your information.... ');
+    FB.api('/me/home', {access_token: accessToken}, function(response) {
+    	scrape(response);
+   	  	console.log(response);
+      	//$('#siteTable').text(response);
+    });
+    FB.api('/me', function(response){
+    	userID = response.id;
+    	console.log('Good to see you, ' + response.name + '.'); // ha you're a number
+    	if(checkIfItemExists(userID, userList))
+    		console.log("Looks like you've been here before. Welcome back.");
+    	else
+    		userList.push(userID);
+    });
+}
+
 function scrape(response){
 	var content = [];
 	for(var i = 0; i < response.data.length; i++)
 	{
-		//console.log(a);
-		//if(response.data[a].hasOwnProperty('source') == true){
 		var thisPost = response.data[i];
 		var author = thisPost.from.name;
 		var postID = thisPost.id;
@@ -38,33 +53,15 @@ function scrape(response){
 			"<div class=\"score likes\">" + (likes + 1) + "</div><div class=\"score unvoted\">" + likes + "</div><div class=\"score dislikes\">" + 
 			(likes - 1) + "</div><div class=\"arrow down login-required\" onclick=\"dislike(this)\" role=\"button\"></div></div>" + 
 			"<p class=\"title\"><a href=\"" + postURL + "\">" + snippet + "</a></p> <div> by " + author + " </div><div class=\"clearleft\"></div>" + "</div>");
-		//content.push("<div>"+ response.data[a].id + "</div> ");
-			//console.log(a +":"+response.data[a].from.name);
-			//console.log(response.data[a].id + '\n');
-			/*
-		}
-		else{
-			content.push("<div>" + a + response.data[a].from.name + "</div> ">);
-			content.push("<div>" + a + response.data[a].id + "</div> ">);
-			//console.log(a+":"+response.data[a].from.name);
-			//console.log(response.data[a].message + '\n');
-		}
-		*/
 	}
 	//console.log(typeof content);
 	$('#siteTable').html(content.join(' '));
-	//console.log(typeof content.join(' '));
 }
 
-function init() {
-    console.log('Welcome!  Fetching your information.... ');
-    FB.api('/me/home', {access_token: accessToken}, function(response) {
-    	scrape(response);
-   	  	console.log(response);
-      	//$('#siteTable').text(response);
-    });
-    FB.api('/me', function(response){
-    	userID = response.id;
-    	console.log('Good to see you, ' + response.name + '.'); // ha you're a number
-    });
+// Tests to see if /users/<userId> has any data. 
+function checkIfItemExists(itemID, ref) {
+  ref.child(itemID).once('value', function(snapshot) {
+    var exists = (snapshot.val() !== null);
+    return exists;
+  });
 }
